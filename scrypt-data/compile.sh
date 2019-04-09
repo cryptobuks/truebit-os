ff5c758739ac51486624d875b2d8726e1b1c9e19
Sami Mäkelä <mrsmkl@gmail.com>
2019-04-09 00:27:25 -0700

    reading VM params from the file seems to work

--- scrypt-data/compile.sh
+++ scrypt-data/compile.sh
@@ -6,7 +6,7 @@ em++ -O2 -c -std=c++11 scrypthash.cpp
 em++ -O2 -c -std=c++11 scrypt.cpp
 em++ -o scrypt.js scrypthash.o scrypt.o -L ~/openssl -lcrypto -lssl
 
-node ~/emscripten-module-wrapper/prepare.js scrypt.js --debug --run --file input.data --file output.data --memory-size 20 --out=stuff --metering=5000 --upload-ipfs --limit-stack
+node ~/truebit-os/emscripten-module-wrapper/prepare.js scrypt.js --debug --run --file input.data --file output.data --memory-size 20 --out=stuff --metering=5000 --upload-ipfs --limit-stack
 
 cp stuff/globals.wasm task.wasm
 cp stuff/info.json .
